e0652fc24f1521363e5a2da7a5ddf2b3227ec2e4
Michal <michal@kronmar.eu>
2021-01-13 19:56:22 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,249 @@
+#include "glew.h"
+#include "freeglut.h"
+#include "glm.hpp"
+#include "ext.hpp"
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+#include "Shader_Loader.h"
+#include "Render_Utils.h"
+#include "Camera.h"
+#include "Texture.h"
+#include "Physics.h"
+
+Core::Shader_Loader shaderLoader;
+GLuint programColor;
+GLuint programTexture;
+
+obj::Model planeModel, shipModel;
+Core::RenderContext planeContext, shipContext;
+GLuint groundTexture;
+
+glm::vec3 cameraPos = glm::vec3(0, 3, 10);
+glm::vec3 cameraDir;
+glm::vec3 cameraSide;
+float cameraAngle = 0;
+glm::mat4 cameraMatrix, perspectiveMatrix;
+
+glm::quat rotation = glm::quat(1, 0, 0, 0);
+
+glm::vec3 lightDir = glm::normalize(glm::vec3(0.5, -1, -0.5));
+
+int oldX = 0, oldY = 0, divX, divY;
+
+
+Physics pxScene(9.8 /* gravity (m/s^2) */);
+
+
+const double physicsStepTime = 1.f / 60.f;
+double physicsTimeToProcess = 0;
+
+PxRigidStatic *planeBody, *shipBody = nullptr;
+PxMaterial *planeMaterial, *shipMaterial = nullptr;
+
+struct Renderable {
+    Core::RenderContext* context;
+    glm::mat4 modelMatrix;
+    GLuint textureId;
+};
+std::vector<Renderable*> renderables;
+
+void initRenderables()
+{
+    planeModel = obj::loadModelFromFile("models/plane.obj");
+    shipModel = obj::loadModelFromFile("models/spaceship.obj");
+
+    planeContext.initFromOBJ(planeModel);
+    shipContext.initFromOBJ(shipModel);
+
+    groundTexture = Core::LoadTexture("textures/sand.jpg");
+
+}
+
+void initPhysicsScene()
+{
+
+}
+
+void updateTransforms()
+{
+    auto actorFlags = PxActorTypeFlag::eRIGID_DYNAMIC | PxActorTypeFlag::eRIGID_STATIC;
+    PxU32 nbActors = pxScene.scene->getNbActors(actorFlags);
+    if (nbActors)
+    {
+        std::vector<PxRigidActor*> actors(nbActors);
+        pxScene.scene->getActors(actorFlags, (PxActor**)&actors[0], nbActors);
+        for (auto actor : actors)
+        {
+            if (!actor->userData) continue;
+            Renderable *renderable = (Renderable*)actor->userData;
+
+            PxMat44 transform = actor->getGlobalPose();
+            auto &c0 = transform.column0;
+            auto &c1 = transform.column1;
+            auto &c2 = transform.column2;
+            auto &c3 = transform.column3;
+
+            renderable->modelMatrix = glm::mat4(
+                c0.x, c0.y, c0.z, c0.w,
+                c1.x, c1.y, c1.z, c1.w,
+                c2.x, c2.y, c2.z, c2.w,
+                c3.x, c3.y, c3.z, c3.w);
+        }
+    }
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+    float angleSpeed = 0.1f;
+    float moveSpeed = 0.1f;
+    switch (key)
+    {
+    case 'z': cameraAngle -= angleSpeed; break;
+    case 'x': cameraAngle += angleSpeed; break;
+    case 'w': cameraPos += cameraDir * moveSpeed; break;
+    case 's': cameraPos -= cameraDir * moveSpeed; break;
+    case 'd': cameraPos += cameraSide * moveSpeed; break;
+    case 'a': cameraPos -= cameraSide * moveSpeed; break;
+    }
+}
+
+void mouse(int x, int y)
+{
+    divX = x - oldX;
+    divY = y - oldY;
+    oldX = x;
+    oldY = y;
+}
+
+glm::mat4 createCameraMatrix()
+{
+    glm::quat x = glm::angleAxis(divX / 50.0f, glm::vec3(0, 1, 0));
+    glm::quat y = glm::angleAxis(divY / 50.0f, glm::vec3(1, 0, 0));
+    glm::quat z = glm::angleAxis(cameraAngle, glm::vec3(0, 0, 1));
+    glm::quat xy = x * y * z;
+    rotation = glm::normalize(xy * rotation);
+
+    divX = 0;
+    divY = 0;
+    cameraAngle = 0;
+
+    cameraDir = glm::inverse(rotation) * glm::vec3(0, 0, -1);
+    cameraSide = glm::inverse(rotation) * glm::vec3(1, 0, 0);
+
+    return Core::createViewMatrixQuat(cameraPos, rotation);
+}
+
+void drawObjectColor(Core::RenderContext* context, glm::mat4 modelMatrix, glm::vec3 color)
+{
+    GLuint program = programColor;
+
+    glUseProgram(program);
+
+    glUniform3f(glGetUniformLocation(program, "objectColor"), color.x, color.y, color.z);
+    glUniform3f(glGetUniformLocation(program, "lightDir"), lightDir.x, lightDir.y, lightDir.z);
+
+    glm::mat4 transformation = perspectiveMatrix * cameraMatrix * modelMatrix;
+    glUniformMatrix4fv(glGetUniformLocation(program, "modelViewProjectionMatrix"), 1, GL_FALSE, (float*)&transformation);
+    glUniformMatrix4fv(glGetUniformLocation(program, "modelMatrix"), 1, GL_FALSE, (float*)&modelMatrix);
+
+    Core::DrawContext(*context);
+
+    glUseProgram(0);
+}
+
+void drawObjectTexture(Core::RenderContext * context, glm::mat4 modelMatrix, GLuint textureId)
+{
+    GLuint program = programTexture;
+
+    glUseProgram(program);
+
+    glUniform3f(glGetUniformLocation(program, "lightDir"), lightDir.x, lightDir.y, lightDir.z);
+    Core::SetActiveTexture(textureId, "textureSampler", program, 0);
+
+    glm::mat4 transformation = perspectiveMatrix * cameraMatrix * modelMatrix;
+    glUniformMatrix4fv(glGetUniformLocation(program, "modelViewProjectionMatrix"), 1, GL_FALSE, (float*)&transformation);
+    glUniformMatrix4fv(glGetUniformLocation(program, "modelMatrix"), 1, GL_FALSE, (float*)&modelMatrix);
+
+    Core::DrawContext(*context);
+
+    glUseProgram(0);
+}
+
+void renderScene()
+{
+    double time = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
+    static double prevTime = time;
+    double dtime = time - prevTime;
+    prevTime = time;
+
+    if (dtime < 1.f) {
+        physicsTimeToProcess += dtime;
+        while (physicsTimeToProcess > 0) {
+            pxScene.step(physicsStepTime);
+            physicsTimeToProcess -= physicsStepTime;
+        }
+    }
+
+    cameraMatrix = createCameraMatrix();
+    perspectiveMatrix = Core::createPerspectiveMatrix();
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0f, 0.1f, 0.3f, 1.0f);
+
+    updateTransforms();
+
+    for (Renderable* renderable : renderables) {
+        drawObjectTexture(renderable->context, renderable->modelMatrix, renderable->textureId);
+    }
+
+    glm::mat4 shipMatrix = glm::translate(cameraPos + cameraDir * 0.5f) * glm::mat4_cast(glm::inverse(rotation)) * glm::translate(glm::vec3(0, -0.25f, 0)) * glm::rotate(glm::radians(180.0f), glm::vec3(0, 1, 0)) * glm::scale(glm::vec3(0.25f));
+    drawObjectTexture(&shipContext, shipMatrix, groundTexture);
+
+    glutSwapBuffers();
+}
+
+void init()
+{
+    srand(time(0));
+    glEnable(GL_DEPTH_TEST);
+    programColor = shaderLoader.CreateProgram("shaders/shader_color.vert", "shaders/shader_color.frag");
+    programTexture = shaderLoader.CreateProgram("shaders/shader_tex.vert", "shaders/shader_tex.frag");
+
+    initRenderables();
+    initPhysicsScene();
+}
+
+void shutdown()
+{
+    shaderLoader.DeleteProgram(programColor);
+    shaderLoader.DeleteProgram(programTexture);
+}
+
+void idle()
+{
+    glutPostRedisplay();
+}
+
+int main(int argc, char ** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(200, 200);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("Space Simulator");
+    glewInit();
+
+    init();
+    glutKeyboardFunc(keyboard);
+    glutPassiveMotionFunc(mouse);
+    glutDisplayFunc(renderScene);
+    glutIdleFunc(idle);
+
+    glutMainLoop();
+
+    shutdown();
+
+    return 0;
+}
